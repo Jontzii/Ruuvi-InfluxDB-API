c49ebbde2e6f6f5d3bb399989df82a80fcf7d699
Joonas Hiltunen <joonas.hiltunen@tuni.fi>
2020-03-10 00:43:43 +0200

    Main functionality completed

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
-# RuuviAPI
+# Influx DB REST API
 REST API that returns values from Ruuvi DB
 
-# Dependecies
+## Flow
+1. Users sends GET request with api key and optional paramters
+2. App checks if given api key is correct from mysql database containing registered keys
+3. If correct app retrieves asked information and sends it pack as a JSON
+
+## Data returned
+1. time - UTC Time of measurement
+2. temperature - Measured temperature
+3. air density - Calculated air density
+4. dew point - Calculated dew point
+5. humidity - Measured humidity
+6. pressure - Measured pressure
+
+## Variables (optional)
+1. api_key - required - API key to use
+2. limit - (optional) - How many results to get, max amount is defined per user in SQL
+3. sortby DESC/ASC - (optional) - Sort results to be descending or ascending
+4. ~~indent~~ - (optional) - indent results to be more readable - Not Yet Implemented
+
+## Dependecies
 - ExpressJS
 - Node-Influx
+- Dotenv
+- MySQL
+- express-slow-down
--- index.js
+++ index.js
@@ -1,52 +1,156 @@
+require('dotenv').config()
 var express = require("express");
-const Influx = require('influx');
+var slowDown = require("express-slow-down");
+var Influx = require('influx');
 var mysql = require('mysql');
 
 var app = express();
 
 var api_con = mysql.createConnection({
-  host: 'localhost',
-  user: 'APIUser',
-  password: 'kEUmoi5Bc5',
-  database: 'ruuvi'
+  host: process.env.SQL_DB_HOST,
+  user: process.env.SQL_DB_USER,
+  password: process.env.SQL_DB_PASSWORD,
+  database: process.env.SQL_DB_DATABASE
 });
 
-const client = new Influx.InfluxDB({
-  database: 'ruuvi',
-  host: 'localhost',
-  port: 8086,
-  username: 'ruuvi',
-  password: 'RuuviTagPW'
+var influx_client = new Influx.InfluxDB({
+  database: process.env.INFLUX_DATABASE,
+  host: process.env.INFLUX_HOST,
+  port: process.env.INFLUX_PORT,
+  username: process.env.INFLUX_USER,
+  password: process.env.INFLUX_PASSWORD
 })
 
+const speedLimiter = slowDown({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	delayAfter: 150, // allow 150 requests per 15 minutes, then...
+	delayMs: 500 // begin adding 500ms of delay per request above 100:
+});
+  
 function checkAPIKey(key, callback) {
-	var queryString = "SELECT api_key FROM RuuviAPIKeys WHERE api_key='" + key + "'";
-	api_con.query(queryString, function (err, result, fields) {
+	var queryString = "SELECT api_key, name, length_limit FROM RuuviAPIKeys WHERE api_key='" + key + "'";
+
+	api_con.query(queryString, function (err, api_result, fields) {
 		if (err) throw err;
 
-		if (result.length > 0) {
-			console.log("Got to true");
+		var length_limit = null;
+
+		if (api_result.length > 0) {
+			length_limit = api_result[0].length_limit;
 			pass = true;
 		} else {
-			console.log("Got to false");
 			pass = false;
 		}
 		
-		callback();
+		callback(api_result, pass, length_limit);
+
 	});
 };
 
-app.get("/weather/api", (req, res, next) => {
-	var key = req.query.api_key;
-	checkAPIKey(key, function() {
-		if (pass == true)
-			res.send("Apikey is set to " + key);
-		else
-			res.send("Not allowed");
+function GetResultsFromInflux(limit, sortBy, callback) {
+	influx_query = 'SELECT temperature, airDensity, dewPoint, humidity, pressure ' + 
+	'FROM ruuvi_outdoor_measurements ORDER BY ' + sortBy.toString();
+	
+	if (limit != -1) {
+		influx_query += ' LIMIT ' + limit.toString();
+	}
+
+	influx_client.query(influx_query).then(data => {
+		console.log(data);
+		callback(data);
 	});
+}
+
+function GenerateResponse(limit, sortBy, api_result, callback) {
+
+	GetResultsFromInflux(limit, sortBy, function(measurements) {
+		var JSON_res = {
+			"status" : "OK",
+			"user" : api_result[0].name,
+			"api_key": api_result[0].api_key,
+			"limit": limit,
+			"sortby": sortBy,
+			"body": []
+		};
+
+		i = 0
+
+		while (measurements[i] != null) {
+			var temporary = {};
+
+			temporary['time'] = measurements[i].time;
+			temporary['temperature'] = measurements[i].temperature
+			temporary['air_density'] = measurements[i].airDensity
+			temporary['dewpoint'] = measurements[i].dewPoint
+			temporary['humidity'] = measurements[i].humidity
+			temporary['pressure'] = measurements[i].pressure
+
+			JSON_res.body[i] = temporary;
+			i++;
+		}
+
+		if (JSON_res == null) {
+			JSON_res = {
+				"status": "Error",
+				"data": []
+			}
+		}
+
+		callback(JSON_res);
+	});
+}
+
+app.use(speedLimiter);
+
+app.get("/weather/apiv1", (req, res, next) => {
+	var key = req.query.api_key;  // API key
+	var limit = req.query.limit;  // limit of results
+	var sortBy = req.query.sort;  // Sort by DESC/ASC
+	var indent = req.query.indent;  // Indent results NOT IMPLEMENTED
+
+	checkAPIKey(key, function(api_result, pass, length_limit) {
+
+		if (limit == {} || limit == null || limit < 1) {
+
+			if (length_limit == null) limit = -1
+			else limit = 1
+
+		} else if ((limit > length_limit) && length_limit != null) {
+			limit = length_limit;
+		}
+
+		if (sortBy != {} && sortBy != null) {
+
+			if (sortBy.toUpperCase() != "ASC") sortBy = "DESC";
+			else sortBy = "ASC";
+
+		} else {
+			sortBy = "DESC";
+		}
+		
+		if (pass == true) {
+			GenerateResponse(limit, sortBy, api_result, function(JSON_res) {
+				res.status(200).type('application/json').json(JSON_res);
+			});
+		}
+		else {
+			res.status(401).send('Unauthorized');
+		}
+	});
+});
+
+app.post('/', function (req, res) {
+	res.status(401).send('Unauthorized');
+});
+
+app.put('/', function (req, res) {
+	res.status(401).send('Unauthorized');
+});
+
+app.delete('/', function (req, res) {
+	res.status(401).send('Unauthorized');
 });
 
 app.listen(3000, () => {
 	console.log("Server running on port 3000");
 });
-
